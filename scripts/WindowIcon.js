a5a7404a1121c9a473e8422284b8e295e00586c3
kokonut <beol0127@gmail.com>
2022-06-11 23:19:50 -0400

    Update WindowIcon.js

--- scripts/WindowIcon.js
+++ scripts/WindowIcon.js
@@ -4,7 +4,9 @@ export default function windowIcon() {
       style={{
         content:
           "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==)",
-        margin: "0 0.1875rem 0 0.3125rem"
+        margin: "0"
+        display: "inline-block"
+        vertical-align: "middle"
       }}
       alt="Opens in a new tab"
     />
