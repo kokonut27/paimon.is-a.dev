0e12924f9e663074cae18d9f3f30d5479e2a41f0
kokonut <beol0127@gmail.com>
2022-06-11 23:38:40 -0400

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
   res.status(200).json({ 
     types: ["artifacts", "bosses", "characters", "consumables", "domains", "enemies", "items", "lands", "weapons"] 
