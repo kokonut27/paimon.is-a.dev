a697a53a738d3ecbd1207d559e36e5204e850ff0
kokonut <beol0127@gmail.com>
2022-06-12 00:03:55 -0400

    feat: add param queries

--- pages/api/artifacts/index.js
+++ pages/api/artifacts/index.js
@@ -1,5 +1,9 @@
 export default function handler(req, res) {
+  const { num, rarity, set } = req.query
+  
+  
   res.status(200).json({ 
-    types: ["number", "rarity", "set"] 
+    types: ["number", "rarity", "set"],
+    num: num, rarity: rarity, set: set
   })
 }
