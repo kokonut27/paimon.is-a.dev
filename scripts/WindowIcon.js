58958f69b87ff4607b403214fc5489d2eff923af
kokonut <beol0127@gmail.com>
2022-06-11 23:13:47 -0400

    Create WindowIcon.js

---
+++ scripts/WindowIcon.js
@@ -0,0 +1,12 @@
+export default function windowIcon() {
+  return (
+    <img
+      style={{
+        content:
+          "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==)",
+        margin: "0 0.1875rem 0 0.3125rem"
+      }}
+      alt="Opens in a new tab"
+    />
+  )
+}
