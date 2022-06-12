d68617fa708fbc4ba0e3447ab6dedd2afa868e5c
kokonut <beol0127@gmail.com>
2022-06-12 00:26:14 -0400

    Update index.js

--- pages/api/artifacts/index.js
+++ pages/api/artifacts/index.js
@@ -2,7 +2,7 @@ import { AllArtifacts, ArtifactsInfo } from '.../data/artifacts'
 
 export default function handler(req, res) {
   const { num, set } = req.query
-  if AllArtifacts.includes(set) {
+  if (AllArtifacts.includes(set)) {
     res.status(200).json({ 
       name: set,
       rarity: ArtifactsInfo["rarity"]
